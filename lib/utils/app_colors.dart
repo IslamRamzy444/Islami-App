1ce462ef18cb8b0b64e43fb443c96df6f0ca9afc
IslamRamzy444 <islamramzy2@gmail.com>
2025-06-24 17:47:14 +0300

    -Make an introduction screen -Intiating an empty home screen

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackBackgroundColor=Color(0xff202020);
+  static const Color primaryColor=Color(0xffE2BE7F);
+  static const Color transparentColor=Colors.transparent;
+  static const Color dotsGreyColor=Color(0xff707070);
+  static const Color dotsActiveColor=Color(0xffFFD482);
+}
